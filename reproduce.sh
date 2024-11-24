2384adfbeb923eec911885d1cec8a8a4c55e3bbc
Rafael Fernández López <ereslibre@ereslibre.es>
2024-11-24 22:54:27 +0100

    WIP

--- reproduce.sh
+++ reproduce.sh
@@ -3,11 +3,11 @@ sudo systemctl start nvidia-container-toolkit-cdi-generator
 sudo systemctl start docker
 docker ps -a
 sudo systemctl stop docker
-sudo systemctl list-units | grep 0d1bbc2c74a1 | awk '{print $1}' | xargs -I{} sudo systemctl stop {}
+sudo systemctl list-units | grep d0c25a7bd68c | awk '{print $1}' | xargs -I{} sudo systemctl stop {}
 sudo systemctl daemon-reload
 sudo systemctl stop nvidia-container-toolkit-cdi-generator
 docker ps -a
 sudo systemctl restart docker
 docker ps -a
-docker inspect 0d1bbc2c74a1 | jq '.[].State'
+docker inspect d0c25a7bd68c | jq '.[].State'
 # docker-compose up -d
