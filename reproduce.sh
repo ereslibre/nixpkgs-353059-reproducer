998cb6955f9f25d0f269f77cb451f1d34b922d70
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-04 17:43:52 +0100

    Update

--- reproduce.sh
+++ reproduce.sh
@@ -9,7 +9,9 @@ docker ps -a
 sudo systemctl stop docker
 sudo systemctl list-units | grep docker | grep scope  | awk '{print $1}' | xargs -I{} sudo systemctl stop {}
 sudo systemctl daemon-reload
+tree /var/run/cdi
 sudo systemctl stop nvidia-container-toolkit-cdi-generator
+tree /var/run/cdi
 docker ps -a
 sudo systemctl restart docker
 while true; do
