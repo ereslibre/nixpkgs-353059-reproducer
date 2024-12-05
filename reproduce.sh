dbb7e37a94535489643c73a875be9a5cf35a85d8
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-05 17:36:34 +0100

    Update

--- reproduce.sh
+++ reproduce.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
-export DOCKER_HOST=unix:///tmp/docker-testing
+export DOCKER_HOST_PATH=/tmp/docker-testing
+export DOCKER_HOST=unix://$DOCKER_HOST_PATH
 
-rm /tmp/docker-testing
-pkill docker
-pkill dockerd
-pkill rootlesskit
-pkill containerd
+kill_docker() {
+    while true; do
+        pkill docker &> /dev/null
+    done
+    while true; do
+        pkill dockerd &> /dev/null
+    done
+    while true; do
+        pkill rootlesskit &> /dev/null
+    done
+    while true; do
+        pkill containerd &> /dev/null
+    done
+    rm /tmp/docker-testing &> /dev/null
+}
 
 run_docker() {
     ~/.wip/dockerd &> output.txt &
-    echo $!
+    RET_PID=$!
+    while true; do
+        if docker ps &> /dev/null; then
+            break
+        fi
+        echo "wait 1"
+        sleep 0.5
+    done
+    echo $RET_PID
 }
 
 docker ps -aq | xargs docker rm -f
@@ -19,15 +38,10 @@ while true; do
     if ls /var/run/cdi; then
         break
     fi
+    echo "wait 2"
     sleep 0.5
 done
 DOCKERD_PID=$(run_docker)
-while true; do
-    if docker ps; then
-        break
-    fi
-    sleep 0.5
-done
 if ! docker ps -a | grep gpu-test-container; then
     docker-compose up -d
 fi
@@ -38,12 +52,6 @@ sudo systemctl stop nvidia-container-toolkit-cdi-generator
 tree /var/run/cdi
 docker ps -a
 DOCKERD_PID=$(run_docker)
-while true; do
-    if docker ps; then
-        break
-    fi
-    sleep 0.5
-done
 docker ps -a
 docker inspect $(docker ps -aq --filter=name=gpu-test-container) | jq '.[].State'
 kill $DOCKERD_PID
