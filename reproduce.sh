07d3d554b31373214574e92458ef5901a3e3e298
Rafael Fernández López <ereslibre@ereslibre.es>
2025-05-15 12:15:17 +0200

    Update

--- reproduce.sh
+++ reproduce.sh
@@ -1,24 +1,67 @@
 #!/usr/bin/env bash
+
+set -x
+
+export DOCKER_HOST_PATH=/tmp/docker-testing
+export DOCKER_HOST=unix://$DOCKER_HOST_PATH
+
+kill_docker() {
+    echo "Killing process $1"
+    kill $1
+    pkill docker
+    pkill dockerd
+    pkill rootlesskit
+    pkill containerd
+    while true; do
+        if ! docker ps &> /dev/null; then
+            break
+        fi
+        sleep 0.5
+    done
+    rm /tmp/docker-testing &> /dev/null
+}
+
+run_docker() {
+    echo "Running docker..." 1>&2
+    pkill rootlesskit
+    ./dockerd &>> output.txt &
+    RET_PID=$!
+    while true; do
+        if docker ps &> /dev/null; then
+            echo "docker is available" 1>&2
+            break
+        fi
+        sleep 0.5
+    done
+    echo $RET_PID
+}
+
+echo "Cleaning up containers"
 docker ps -aq | xargs docker rm -f
+
+echo "Starting nvidia-container-cdi-generator"
 sudo systemctl start nvidia-container-toolkit-cdi-generator
-sudo systemctl start docker
-if ! docker ps -a | grep gpu-test-container; then
-    docker-compose up -d
-fi
-docker ps -a
-sudo systemctl stop docker
-sudo systemctl list-units | grep docker | grep scope  | awk '{print $1}' | xargs -I{} sudo systemctl stop {}
-sudo systemctl daemon-reload
-tree /var/run/cdi
-sudo systemctl stop nvidia-container-toolkit-cdi-generator
-tree /var/run/cdi
-docker ps -a
-sudo systemctl restart docker
 while true; do
-    if docker ps; then
+    echo "  Waiting for /var/run/cdi to be present"
+    if ls /var/run/cdi; then
+        echo "/var/run/cdi present"
         break
     fi
     sleep 0.5
 done
+DOCKERD_PID=$(run_docker)
+if ! docker ps -a | grep gpu-test-container; then
+    echo "gpu-test-container not present, bringing up"
+    docker run --name=gpu-test-container --restart=always --entrypoint='/bin/sh' --device=nvidia.com/gpu=all ubuntu:latest -c nvidia-smi -L && sleep infinity
+fi
+docker ps -a
+kill_docker $DOCKERD_PID
+tree /var/run/cdi
+sudo systemctl stop nvidia-container-toolkit-cdi-generator
+tree /var/run/cdi
+docker ps -a
+DOCKERD_PID=$(run_docker)
 docker ps -a
 docker inspect $(docker ps -aq --filter=name=gpu-test-container) | jq '.[].State'
+kill_docker $DOCKERD_PID
+echo "> Waiting for dockerd stop (PID: $DOCKERD_PID)"
